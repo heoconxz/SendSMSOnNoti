87e369f80f82ee9f43626a47c4284c80cddf23de
TungTT26 <TungTT26@vingroup.net>
2020-01-21 15:54:24 +0700

    Add ask for permission

--- app/src/main/java/com/example/sendsmsonnoti/MainActivity.kt
+++ app/src/main/java/com/example/sendsmsonnoti/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.sendsmsonnoti
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.firebase.messaging.FirebaseMessaging
 
@@ -17,6 +22,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (checkSelfPermission(this,Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS),0)
+        }
         FirebaseMessaging.getInstance().subscribeToTopic("SMS")
             .addOnCompleteListener { task ->
                 var msg = getString(R.string.msg_subscribed)
@@ -30,6 +40,21 @@ class MainActivity : AppCompatActivity() {
         lbm.registerReceiver(broadcastReceiver, IntentFilter(R.string.log_intent_string.toString()))
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode)
+        {
+            0->{
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {}else{
+                    finishAndRemoveTask()
+                }
+            }
+        }
+    }
     val broadcastReceiver = object: BroadcastReceiver(){
         override fun onReceive(context: Context?,intent:Intent ){
             if (intent != null){
